35b187f498adceee3716e686ec4b9fbb66a14cc4
maerdogan <maerdo@gmail.com>
2016-02-21 10:41:11 +0100

    3 new files, header, index, suggest

---
+++ index.php
@@ -0,0 +1,54 @@
+<html>
+<head>
+	<title>Die Amis</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+
+	<div class="header">
+
+		<div class="wrapper">
+
+			<h1 class="header-title"><a href="./">Die Amis</a><h1>
+
+			<ul class="nav">
+				<li class="Home"><a href="index.php">Home</a></li>
+				<li class="Dictionary"><a href="dictionary.php">Dictionary</a>
+				<li class="Translation"><a href="tanslation.php">Dictionary</a>
+				<li class="Grammar"><a href="grammar.php">Dictionary</a>
+				<li class="Lessons"><a href="lessons.php">Dictionary</a>
+				<li class="Tests"><a href="tests.php">Dictionary</a>
+			</ul>
+
+		</div>
+
+	</div>
+
+<div id="content">
+	<div class="section catalog random">
+
+		<div class="wrapper">
+
+			<p>lorem ipsum</p>
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="footer">
+	<div class="wrapper">
+
+		<ul>
+			<li><a href="http://www.">Impressum</a></li>
+			<li><a href="http://www.">Kontakt</a></li>
+			<li><a href="http://www.">Facebook</a></li>
+		</ul>
+
+	</div>
+
+</div>
+
+</body>
+</html>
